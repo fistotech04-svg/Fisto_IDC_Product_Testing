55784d9e133987b05b489a47942ea62aed3f4ca0
fistotech04-svg <fistotech04@gmail.com>
2026-05-03 16:00:11 +0530

    Change MongoDB database name to 'Fisto_IDC_Test'

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            dbName: 'Fisto_IDC'
+            dbName: 'Fisto_IDC_Test'
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
